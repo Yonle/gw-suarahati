7f64a369faddb230dbc2326db25301173aa258e2
Yonle <yonle@lecturify.net>
2024-09-19 17:51:59 +0700

    refactor code for request with Authorization header
    Signed-off-by: Yonle <yonle@lecturify.net>

--- mastodon.go
+++ mastodon.go
@@ -71,6 +71,17 @@ func isTextTooLong(text string) bool {
 	return int64(len(text)) > instance.Configuration.Statuses.Max_Characters
 }
 
+func masto_make_request(method string, path string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest(method, config.Mastodon_Host_Url+path, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", auth)
+
+	return req, err
+}
+
 func keluarkan(text string, mediaID *string, spoiler *bool) (*http.Response, error) {
 	n := Note{
 		Status: text,
@@ -87,23 +98,21 @@ func keluarkan(text string, mediaID *string, spoiler *bool) (*http.Response, err
 	j := bytes.Buffer{}
 	json.NewEncoder(&j).Encode(n)
 
-	req, err := http.NewRequest("POST", config.Mastodon_Host_Url+"/api/v1/statuses", &j)
+	req, err := masto_make_request("POST", "/api/v1/statuses", &j)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", "application/json")
 
 	return hc.Do(req)
 }
 
 func masto_postMultipart(mp *multipart.Writer, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest("POST", config.Mastodon_Host_Url+"/api/v2/media", body)
+	req, err := masto_make_request("POST", "/api/v2/media", body)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", mp.FormDataContentType())
 
 	return hc.Do(req)
@@ -122,4 +131,31 @@ func getPostURL(resp *http.Response) (string, error) {
 	}
 
 	return status.URL, nil
+}
+
+func masto_wait_media_ready(id string) error {
+	for {
+		req, err := masto_make_request("GET", "/api/v1/media/"+id, nil)
+		if err != nil {
+			return err
+		}
+
+		resp, err := hc.Do(req)
+		if err != nil {
+			return err
+		}
+
+		resp.Body.Close()
+
+		if resp.StatusCode == 206 {
+			time.Sleep(time.Second * 2)
+			continue
+		}
+
+		if resp.StatusCode >= 400 {
+			return errors.New(fmt.Sprintf("Status code: %d", resp.StatusCode))
+		}
+
+		return nil
+	}
 }
