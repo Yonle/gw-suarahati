5bedb485d545b875c9af305a24bc59ce6e556df5
Yonle <yonle@lecturify.net>
2024-09-19 19:57:57 +0700

    form: don't panic on failed write
    Signed-off-by: Yonle <yonle@lecturify.net>

--- form.go
+++ form.go
@@ -5,12 +5,12 @@ import (
 	"mime/multipart"
 )
 
-func writeToMultipart(filename string, f io.Reader, mp *multipart.Writer, w *io.PipeWriter) {
+func writeMultipartBody(filename string, f io.Reader, mp *multipart.Writer, w *io.PipeWriter) {
 	defer w.Close()
 	defer mp.Close()
 	fileWriter, err := mp.CreateFormFile("file", filename)
 	if err != nil {
-		panic(err)
+		return
 	}
 	io.Copy(fileWriter, f)
 }
@@ -19,7 +19,7 @@ func createForm(filename string, f io.Reader) (*multipart.Writer, *io.PipeReader
 	r, w := io.Pipe()
 	mp := multipart.NewWriter(w)
 
-	go writeToMultipart(filename, f, mp, w)
+	go writeMultipartBody(filename, f, mp, w)
 
 	return mp, r, nil
 }
